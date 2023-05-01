1793f2bfbdf2367f0e84fb5cf264ea55cc096b41
Leonard Elevich <leonard.elevich@microchip.com>
2023-05-01 13:22:44 -0700

    Initial rev

---
+++ apps/iperf_demo/firmware/pic32cx_sg41_cult_freertos.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32cx_sg41_cult_freertos configuration
+CND_ARTIFACT_DIR_pic32cx_sg41_cult_freertos=dist/pic32cx_sg41_cult_freertos/production
+CND_ARTIFACT_NAME_pic32cx_sg41_cult_freertos=pic32cx_sg41_cult_freertos.X.production.hex
+CND_ARTIFACT_PATH_pic32cx_sg41_cult_freertos=dist/pic32cx_sg41_cult_freertos/production/pic32cx_sg41_cult_freertos.X.production.hex
